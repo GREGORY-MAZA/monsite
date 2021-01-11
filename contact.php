d90f31ebba77334777583cbbf3d00dd7dec30aab
Greg MAZA <gregory.maza@le-campus-numerique.fr>
2021-01-11 11:57:01 +0100

    for all * integrated require in php files

---
+++ contact.php
@@ -0,0 +1,38 @@
+<?php
+require 'header.php';
+?>
+
+<main id="MAINFORM">
+
+    <h3>CONTACT</h3>
+
+    <form action="https://httpbin.org/post" method="post">
+
+
+        <fieldset>
+
+            Civilité :<br />
+            <input type="radio" name="civi" value="Mme" /> Madame
+            <input type="radio" name="civi" value="Mlle" /> Mademoiselle
+            <input type="radio" name="civi" value="Mr" /> Monsieur <br>
+
+            Votre Nom :<br />
+            <input type="text" name="prenom"/><br>
+
+            Vous êtes :<br />
+            <select name="profil" style="width: 300px; height: 40px;">
+                <option value="parti">Un particulier</option>
+                <option value="profe">Un professionnel</option>
+                <option value="insti">Un institutionnel</option>
+            </select><br>
+
+            <textarea name="le-message" rows="10" cols="70"></textarea>
+            <input type="submit" name="Envoyer" style="width: 100px; height: 40px;">
+
+        </fieldset>
+    </form>
+</main>
+
+<?php
+require 'footer.php';
+?>
--- index.php
+++ index.php
@@ -1,3 +1,106 @@
 <?php
-echo "HELLO CAMILLE !"
+require 'header.php';
+?>
+
+<main>
+
+
+    <article id="contener-Présentation">
+        <h3>Présentation</h3>
+        <p>Bonjour à vous et merci de m’offrir un peu de votre temps afin que vous  puissiez  apprendre  à  mieux  me  connaître.  Cet  exercice  est  toujours un peu compliqué si l’on s’y prend mal. J’espère que vous arriverez à apercevoir mon ressenti sur la future aventure que nous pourrions effectuer ensemble.<br><p>Si  je  devais  résumer  en  une  phrase  l’idée  que  j’ai  du  métier <span class="rouge"> :«  VERS L’INFINI ET AU-DELÀ ! » </span> comme dirait un certain ami de mes enfants.On y trouve un éventail de possibilités immenses, un apprentissage de compétences quasi sans limite où l’ennui est sûrement inexistant pour une personne ayant l’envie d’y consacrer sa vie professionnelle.Ayant vécu déceptions et expériences sans grand intérêt, je cherche maintenant et avant tout une stabilité dans un domaine enrichissant, passionnant,  qui  me  servira  pour  avancer  concrètement  vers  un  projet final qui sera pour ma part l’épanouissement d’une vie.<p>Je n’ai pas peur de me salir les mains comme on dit. J’ai toujours fait en sorte de persévérer dans les domaines qui  pouvaient  être  passionnants  à  mes  yeux. Étant  musicien  autodidacte,  j’ai  passé  des  heures  à  chercher  comment  améliorer  mon  expérience  via  des  cours  sur  internet,  en  achetant  des  livres,  en  partageant  avec  d’autres musiciens ou même simplement en regardant et analysant des vidéos.J’y retrouve le même engouement avec le développement : le plaisir et la gratitude de réussir, l’envie d’aller toujours  plus  loin  dans  l’apprentissage.  L’esprit  d’équipe  est  un  élément  important  à  mes  yeux,  j’aime  me  rendre utile et le partage de savoir est un véritable atout pour la réussite d’une entreprise. Le chemin ne sera certainement pas facile, mais je continuerai d’avancer quoi qu’il arrive.<p>Mon projet professionnel est assez flou pour le moment, beaucoup de chemins s’offrent à nous et chaque choix raconte une histoire différente. On ne juge pas un livre à sa couverture, il faudra donc un peu de temps et de conseils pour choisir la plus belle aventure qui soit et réussir cette quête, qui je l’espère, à vos côtés se passera.Dans l’attente de nouvelles de votre part, je reste à votre entière disposition.<p>Bien cordialement,</p>
+    </article>
+
+    <article id="contener-Compétences">
+        <h3>Compétences</h3>
+        <ul>
+            <li>Developpement web/HTML5 CSS3</li>
+            <li>Analyse informatique</li>
+            <li>Préparation de poste informatique</li>
+            <li>Dépannage hardware/software</li>
+            <li>Microsoft Activ Directory</li>
+            <li>Création/Droits accés</li>
+            <li>Montage vidéo/audio</li>
+        </ul>
+    </article>
+
+    <article id="contener-Hobbies">
+        <h3>Hobbies</h3>
+        <ol>
+            <li>INFORMATIQUE/DOMOTIQUE</li>
+            <li>ASTRONOMIE/SCIENCE</li>
+            <li>PRODUITS HIGH-TECH</li>
+            <li>UNIVERS MUSICALE/CONCERTS</li>
+            <li>COMPOSITEUR MUSICALE</li>
+            <li>GUITARE/PIANO</li>
+            <li>SPORT MECANIQUE MOTO</li>
+            <li>ENIGME/LOGIQUE</li>
+            <li>SPECTACLE/HUMOUR</li>
+            <li>BIERE/BARBECUE</li>
+        </ol>
+    </article>
+
+    <article id="contener-Expériences">
+        <h3>Expériences</h3>
+        <table>
+            <tr>
+                <th>Entreprise</th>
+                <th>POSTE</th>
+                <th>LIEUX</th>
+                <th>DATE</th>
+            </tr>
+            <tr>
+
+                <td>Campus numérique</td>
+                <td>Formation Web developpeur</td>
+                <td>Valence</td>
+                <td>2020-Novembre</td>
+            </tr>
+
+            <tr>
+                <td>ECONOCOM</td>
+                <td>Analyste Informatique/Technicien Back Office.</td>
+                <td>Valence</td>
+                <td>2018-2019</td>
+            </tr>
+
+            <tr>
+                <td>DACD</td>
+                <td>Magasinier/Préparateur de commande/Cariste</td>
+                <td>Saint Marcel les Valence</td>
+                <td>2009-2017</td>
+            </tr>
+
+            <tr>
+                <td>ADECCO/ADIA</td>
+                <td>Missions intérimaires-Divers/manutentionnaire</td>
+                <td>Drôme Ardèche Isère</td>
+                <td>2007-2009</td>
+            </tr>
+
+            <tr>
+                <td>Entreprise BRION</td>
+                <td>Vendeur/Magasinier</td>
+                <td>Romans sur Isère</td>
+                <td>2006-2007</td>
+            </tr>
+
+            <tr>
+                <td>SACER</td>
+                <td>CAP-Canalisateur</td>
+                <td>Le Pouzin</td>
+                <td>2003-2004</td>
+            </tr>
+
+            <tr>
+                <td>Entreprise PADILLA</td>
+                <td>BAC Professionnel-Thermique</td>
+                <td>Romans sur Isère</td>
+                <td>2002 - 2003</td>
+            </tr>
+        </table>
+    </article>
+</main>
+
+<?php
+require 'footer.php';
 ?>
